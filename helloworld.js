890fd0f1eda2243f1051a56cc481531be684159d
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-08 13:53:37 +0530

    writing in console

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+console.log('hello world');
+
+// If you add the %c right after the " character, you can then style the console output by adding the , character after the second ", and then, inside another pair of " and " characters, use valid CSS code to style the words you want to output in the console.
+console.log("%cHello, World", "color: blue; font-size: 40px");
+
+// Output multiple words into the console
+console.log('hello'+ 'there!');
