4b8f68713a9430f3070fe2812bb8df4b561bbb43
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-08 14:05:35 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,31 @@
+var person = "john"
+console.log(person)
+
+var greeting = "good morning"
+console.log(greeting, person)
+
+// changing variable values
+greeting = "hi"
+person = "james"
+console.log(greeting,person)
+
+// task
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog)
+console.log(petCat)
+
+console.log("My pet dog's name is:", petDog)
+console.log("My pet cat's name is:", petCat)
+
+var catSound = "purr"
+var dogSound = "woof"
+
+console.log(petDog, "says", dogSound)
+console.log(petCat, "says", catSound)
+
+catSound = "meow"
+console.log(petCat, "says", catSound)
+
+var petDog = 'Rex'; // Task 1 solution
+console.log("My pet dog's name is: " + petDog);
