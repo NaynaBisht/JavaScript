cd081cb78dbb97583de532f6c680f0d937b73ee5
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-17 13:13:49 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,20 @@
+function myFunc(){
+    console.log("Hello World");
+}
+myFunc()
+
+
+function addTwoNums(){
+    var a = 10
+    var b = 20
+    var c = a+b
+    console.log(c)
+}
+addTwoNums()
+
+function add(a,b){
+    var c = a+b
+    console.log(c)
+}
+add(2,3)
+add(20,30)
