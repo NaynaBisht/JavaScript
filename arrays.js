57d152a113e84d21e9d57513e299389a8fcd3416
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-18 09:04:53 +0530

    strings

--- arrays.js
+++ arrays.js
@@ -1,2 +1,20 @@
 var train1 = ["wheat","barley","potato"]
 console.log(train1[0])
+
+var clothes = []
+clothes.push("sweater")
+clothes.push("skirt")
+clothes.push("jeans")
+clothes.push("shirt")
+clothes.push("top")
+console.log(clothes)
+clothes.pop()
+console.log(clothes)
+clothes.push("scarf")
+console.log(clothes)
+console.log(clothes[2])
+
+var favCar = {}
+favCar.color = "black"
+favCar.convertible = "TRUE"
+console.log(favCar)
---
+++ strings.js
@@ -0,0 +1,37 @@
+var letters = ['a','b','c']
+for(i=0; i<letters.length; i++){
+    console.log(letters[i])
+}
+
+// or iterate over strings
+var string = "doggy"
+for(i=0; i<string.length; i++){
+    console.log(string[i])
+}
+
+var veggies = ["onion","parsley","carrot"]
+console.log(veggies[0])
+
+var fruits = "apple"
+console.log(fruits[0])
+
+// string != array
+var greet = "hello"
+var user = "paul"
+// console.log(greet.pop()) - all array method dont work
+
+console.log(greet+user)
+console.log(greet.concat(user))
+console.log(greet.length)
+console.log(greet.charAt(0))
+console.log("wo".concat("rl").concat("d"))
+console.log(greet.indexOf("t"))
+console.log(greet.indexOf("o"))
+console.log("ho-ho-ho".split("-"))
+console.log(greet.toUpperCase())
+console.log(greet.toLowerCase())
+
+
+
+
+
