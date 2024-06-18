3e6a4d61dcd3a4c6180c309f0584ec54f627de34
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-18 09:24:04 +0530

    error and types

---
+++ errors.js
@@ -0,0 +1,16 @@
+function addNums(a,b){
+    console.log(a+b)
+}
+addNums(2,3)
+addNums("4",5) // treats as string and concatinates this is called bug
+
+// reference error : c and d are not defined
+console.log(c+d)
+console.log("this line never runs")
+
+// syntax error
+var c = "hey
+console.log(c)
+
+// type error
+(5).pop
