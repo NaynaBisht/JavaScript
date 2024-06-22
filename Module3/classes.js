abc1b587fedf342bbd70a39f7e9909f8eb580d94
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-22 22:37:10 +0530

    classes

--- Module3/classes.js
+++ Module3/classes.js
@@ -90,3 +90,37 @@ var baldEagle = new Eagle()
 var kingPenguin = new Penguin()
 baldEagle.useWings()
 kingPenguin.useWings()
+
+
+class StationaryBike {
+    constructor(position, gears) {
+        this.position = position
+        this.gears = gears
+    }
+}
+
+class Treadmill {
+    constructor(position, modes) {
+        this.position = position
+        this.modes = modes
+    }
+}
+
+class Gym {
+    constructor(openHrs, stationaryBikePos, treadmillPos) {
+        this.openHrs = openHrs
+        this.stationaryBike = new StationaryBike(stationaryBikePos, 8)
+        this.treadmill = new Treadmill(treadmillPos, 5)
+    }
+}
+var boxingGym = new Gym("7-22", "right corner", "left corner")
+
+console.log(boxingGym.openHrs) 
+console.log(boxingGym.stationaryBike) 
+console.log(boxingGym.treadmill) 
+
+
+
+
+
+
