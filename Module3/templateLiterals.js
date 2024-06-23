c3d2c471e7f39efbdd3bd2356cbaebe3dfd1cc0f
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-23 20:45:07 +0530

    templateLiteral

---
+++ Module3/templateLiterals.js
@@ -0,0 +1,35 @@
+let greet = "hello"
+let place = "pune"
+console.log(greet);
+console.log(place);
+console.log(greet,place);
+console.log('${greet} ${place}');
+console.log(greet + " " + place + "!")
+
+console.log(`${1 + 1 + 1 + 1 + 1} stars!`);
+
+// console.log("hey 
+//     there") gives error
+
+
+// use backticks and you can use template literal
+let multiline = `hey
+there
+where 
+are`
+console.log(multiline);
+
+let first = `hey there! he said : you werent coming!`
+let second = `ohh! did he?`
+console.log(`${first} - isnt enough ${second}`);
+
+
+
+
+
+
+
+
+
+
+
