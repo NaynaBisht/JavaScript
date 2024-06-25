790f0fdf6ecb2b9a523626fe13ca8d64b577007c
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-25 12:08:06 +0530

    DOM manipulation

---
+++ Modules/dom.js
@@ -0,0 +1,15 @@
+const h1 = document.createElement('h1')
+h1.innerText = "this is an h1 heading"
+
+h1.setAttribute('id', 'sub-heading')
+h1.setAttribute('class', 'heading')
+
+document.body.appendChild(h1)
+
+
+
+
+
+
+
+
