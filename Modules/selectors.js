8786b2cac47536472bfd039d47cc732abc68c260
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-25 16:05:25 +0530

    selector statements

---
+++ Modules/selectors.js
@@ -0,0 +1,26 @@
+// js selectors work with the document object 
+
+//  query selector method - to locate objects inside html
+document.querySelector('p')
+document.querySelector('a')
+document.querySelectorAll('p')
+document.getElementById('para')
+document.getElementsByClassName('heading')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
