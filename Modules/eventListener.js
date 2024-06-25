1c1a68a8c892652c3da1025972554d9f4572ab34
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-06-25 16:15:34 +0530

    event listeners

---
+++ Modules/eventListener.js
@@ -0,0 +1,24 @@
+// click anywhere in the body
+const target = document.querySelector('body')
+function handleClick() {
+    console.log('clicked the body')    
+}
+target.addEventListener('click', handleClick)
+
+function handleClick2(){
+    console.log('clicked the heading')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
